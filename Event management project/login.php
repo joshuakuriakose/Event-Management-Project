27a20ce1a6b35aba722a2bdfacdda60df7d63955
Joshua Kuriakose <156885880+joshuakuriakose@users.noreply.github.com>
2025-03-21 19:39:38 +0530

    Add files via upload

---
+++ Event management project/login.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+    <style>
+*{
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+}
+
+body{
+    font-family: "Lato", sans-serif;
+}
+
+.outer-box{
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(to top left, #003946,#65eafc);
+}
+
+.inner-box{
+    width: 400px;
+    padding: 20px 40px;
+    margin: 0 auto;
+    position: relative;
+    top: 40%;
+    transform: translateY(-50%);
+    background: linear-gradient(to top left,#ffffffea,#ffffff33);
+    backdrop-filter: blur(5px);
+    border-radius: 8px;
+    box-shadow:4px 4px 5px rgba(46, 49, 53, 0.634);
+    z-index: 2;
+}
+
+.signup-header h1{
+    font-size: 2.5rem;
+    color: 212121;
+}
+.signup-header p{
+    font-size: 0.9rem;
+    color: #555;
+}
+
+.signup-body{
+    margin: 20px 0;
+}
+
+.signup-body p{
+    margin: 10px 0;
+}
+
+.signup-body p label{
+    display: block;
+    font-weight: bold;
+}
+
+.signup-body p input{
+    width: 100%;
+    padding: 15px;
+    border: 2px solid #ccc;
+    border-radius: 25px;
+    font-size: 1rem;
+    margin-top: 4px;
+}
+
+/*or we can declare a new id that is defined, #submit{}*/
+.signup-body p input[type="submit"]{
+    border: none;
+    /* background-image: linear-gradient(to right,red,black); */
+    color: white;
+    cursor: pointer;
+    transition: 0.4s ease-in-out;
+    background-size: 200% ;
+    background-image: linear-gradient(to right, #3498db , #002236 );
+}
+
+.signup-body p input[type="submit"]:hover{
+    background-position: -100% 0;
+    box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2);
+    transform: scale(1.05);
+}
+
+.signup-footer p{
+    color: #555;
+    text-align: center;
+}
+
+.signup-footer p a{
+    display: block;
+    color: #3498db;
+    text-decoration: none;
+}
+.signup-footer p a:link{color: blue;}
+.signup-footer p a:visited{color: #3498db;}
+.signup-footer p a:hover{color: #6b00b8;}
+.signup-footer p a:active{color: red;}
+
+.circle{
+    width: 200px;
+    height: 200px;
+    border-radius: 100px;
+    background: linear-gradient(to top right,#ffffff22,#ffffffff);
+    position: fixed;
+}
+
+.c1{
+    top: 100px;
+    left: 40px;
+}
+.c2{
+    bottom: 200px;
+    right: 50px;
+}
+.signup-footer p{
+    color: #555;
+    text-align: center;
+}
+</style>
+</head>
+<body>
+    
+<div class="outer-box">
+    <div class="inner-box">
+        <header class="signup-header">
+            <h1>Login</h1>
+            <p>It just takes 30 seconds</p>
+        </header>
+        <main class="signup-body">
+<form action = login_process.php method="POST">
+            <p>
+                <label for="fullname">Username</label>
+                <input type="text" id="fullname" placeholder="Enter your name" required>
+            </p>
+            <p>
+            <p>
+                <label for="password">Your password</label>
+                <input type="password" id="password" placeholder="At least 8 characters" required>
+            </p>
+            <p>
+                <input type="submit" id="submit" value="Login">
+            </p>
+        </form>
+        </main>
+        <footer class="signup-footer">
+            <p>Don't have an account?
+                <a href="signup.php">SignUp</a>
+            </p>
+        </footer>
+
+    </div>
+    <div class="circle c1"></div>
+    <div class="circle c2"></div>
+</div>
+</form>
+</body>
+</html>
